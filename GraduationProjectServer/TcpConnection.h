5437fa7f161bff6e1516e8c5bc19b6ceda39ed75
Mabangjin <mabangjin@outlook.com>
2019-05-15 20:55:44 +0800

    添加openssl库 添加log4z库

--- GraduationProjectServer/TcpConnection.h
+++ GraduationProjectServer/TcpConnection.h
@@ -1,8 +1,15 @@
 #pragma once
+#include <uv.h>
+#define DEFAULT_PORT 19999
 class TcpConnection
 {
+	uv_loop_t* loop;
+	uv_tcp_t *server;
+	sockaddr_in addr;
 public:
 	TcpConnection();
 	~TcpConnection();
+	int run();
+	//void connectionListen(uv_stream_t* server, int status);
 };
 
